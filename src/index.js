c54317ed04dd7e87f13f7184c8c6288f548d7a43
DevAlexeyProtasenya <283fg56j98@gmail.com>
2021-02-25 17:54:02 +0300

    feat: add max? min? average function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  if(!!array && array.length !== 0){
+    min = array[0];
+    array.forEach(element => {
+      min = (min > element) ? element : min;
+    });
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  if(!!array && array.length !== 0){
+    max = array[0];
+    array.forEach(element => {
+      max = (max < element) ? element : max;
+    });
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  if(!!array && array.length !== 0){
+    sum = 0;
+    array.forEach(element => {
+      sum += element;
+    });
+    avg = sum / array.length; 
+  }
+  return avg;
 }
